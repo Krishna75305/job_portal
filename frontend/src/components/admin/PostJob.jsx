11e20a0894c4ad6510490b574f4fa0ab7bcc6150
Krishna75305 <121679231+Krishna75305@users.noreply.github.com>
2025-11-14 12:07:13 +0530

    Update PostJob.jsx

--- frontend/src/components/admin/PostJob.jsx
+++ frontend/src/components/admin/PostJob.jsx
@@ -52,26 +52,7 @@ const PostJob = () => {
     }
   };
 
-  // const submitHandler = async (e) => {
-  //   e.preventDefault();
-  //   try {
-  //     setLoading(true);
-  //     const res = await axios.post(`${JOB_API_END_POINT}/post`, input, {
-  //       headers: {
-  //         "Content-Type": "application/json",
-  //       },
-  //       withCredentials: true,
-  //     });
-  //     if (res.data.success) {
-  //       toast.success(res.data.message);
-  //       navigate("/admin/jobs");
-  //     }
-  //   } catch (error) {
-  //     toast.error(error?.response?.data?.message || "Job post failed");
-  //   } finally {
-  //     setLoading(false);
-  //   }
-  // };
+ 
 
  const submitHandler = async (e) => {
   e.preventDefault();
